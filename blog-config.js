a43fa54adf7d833cfbe924f4a897c3b7167b4a3e
최준호 <junho5336@gmail.com>
2023-07-08 17:55:12 +0900

    Initial commit

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "pium.log",
+  description: "우아한테크코스 5기 피움팀 기술 블로그입니다.",
+  author: "pium",
+  siteUrl: "https://pium-official.github.io/",
+  links: {
+    github: "https://github.com/woowacourse-teams/2023-pium",
+    instagram: "https://www.instagram.com/official_pium"
+  },
+  utterances: {
+    repo: "pium-official/pium-official.github.io",
+    type: "pathname",
+  },
+};
